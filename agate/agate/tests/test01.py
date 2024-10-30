e7ffcb79c443c84c3876b4e9d2454ec910c28b0a
Neil Butcher <n.a.butcher@bham.ac.uk>
2024-10-30 15:02:05 +0000

    fixed test error

--- agate/agate/tests/test01.py
+++ agate/agate/tests/test01.py
@@ -1,11 +1,14 @@
 
 from django.test import TestCase
-from agate.models import IngestionAttempt    
+from agate.models import IngestionAttempt
 
 
 class IngestionAttemptTestCase(TestCase):
     def setUp(self):
-        IngestionAttempt.objects.create(uuid="a")
+        IngestionAttempt.objects.create(
+            uuid="a",
+            is_published=True,
+            is_test_attempt=False)
 
     def test_ingestion_attempt_created(self):
         attempt = IngestionAttempt.objects.get(uuid="a")
