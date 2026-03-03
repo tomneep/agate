ed7a3b56b690ea472d5b9d760e663ef20ba69b16
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2026-03-03 14:18:53 +0000

    Fix message import

--- agate/core/production.py
+++ agate/core/production.py
@@ -1,5 +1,5 @@
 import os
-from empty_message_retrieval import EmptyMessageRetrieval
+from varys_message_retrieval import VarysMessageRetrieval
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
